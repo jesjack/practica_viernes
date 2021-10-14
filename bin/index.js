9f6c2a68120b2b672ce7345967e2de2ba99e1ac2
jesjack <jesjack25_03@hotmail.com>
2021-10-14 16:31:15 -0500

    Fix file location

--- bin/index.js
+++ bin/index.js
@@ -7,15 +7,16 @@ exports.addRow = exports.setMathFunction = exports.render = void 0;
 var html_pdf_1 = __importDefault(require("html-pdf"));
 var ejs_1 = __importDefault(require("ejs"));
 var fs_1 = require("fs");
+var path_1 = require("path");
 var mathFunction;
 var rows = [];
 function render() {
-    html_pdf_1.default.create(ejs_1.default.render((0, fs_1.readFileSync)(__dirname + '/src/table.ejs').toString(), {
+    html_pdf_1.default.create(ejs_1.default.render((0, fs_1.readFileSync)((0, path_1.join)(__dirname + '/src/table.ejs')).toString(), {
         mathFunction: mathFunction,
         rows: rows
     }), {
         format: 'A5'
-    }).toFile(__dirname + '/output.pdf', function (err) {
+    }).toFile((0, path_1.join)(__dirname + '/output.pdf'), function (err) {
         if (err)
             console.error(err);
     });
