3d8f7bf7cb773db049d88ba984f1f9c793cf0e13
jesjack <jesjack25_03@hotmail.com>
2021-10-14 16:29:41 -0500

    Fix file location

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,17 @@
 import html_to_pdf from 'html-pdf'
 import ejs from 'ejs'
 import { readFileSync } from 'fs'
+import { join } from 'path'
 
 var mathFunction: string
 var rows: Array<[x: string | number, f: string | number]> = []
 
 export function render() {
-  html_to_pdf.create(ejs.render(readFileSync(__dirname + '/src/table.ejs').toString(), {
+  html_to_pdf.create(ejs.render(readFileSync(join(__dirname + '/src/table.ejs')).toString(), {
     mathFunction, rows
   }), {
     format: 'A5'
-  }).toFile(__dirname + '/output.pdf', (err) => {
+  }).toFile(join(__dirname + '/output.pdf'), (err) => {
     if(err) console.error(err)
   })
 }
