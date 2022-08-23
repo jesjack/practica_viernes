b879c481b7f1de02f9657060a86de6be80a6bde8
jesjack <44591326+jesjack@users.noreply.github.com>
2022-08-23 13:28:34 -0500

    c++

---
+++ pr1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main (void)
+{
+    char nombre[100];
+    cout<< "Escribe tu nombre"<<end1;
+    cin>>nombre;
+    cout<< "Tu nombre es "<<nombre <<end1;
+    getche();
+    return 0;
+}
